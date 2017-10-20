9b01a1999b1acadee2830dc3acf0405a3549f431
Zoe Nolan <zn@clearmatics.com>
2017-10-20 10:06:20 +0100

    Reset RPc port back to testRPC

--- truffle.js
+++ truffle.js
@@ -2,7 +2,7 @@ module.exports = {
   networks: {
     development: {
       host: "localhost",
-      port: 8545,
+      port: 8550,
       network_id: "*" // Match any network id
     },
     ci: {
