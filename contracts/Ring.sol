bdd7c37d5a30ca89eddaa49fb24d1a2c594183a1
Zoe Nolan <zn@clearmatics.com>
2017-11-14 14:00:06 +0000

    Move the ERC20 interface to a separate file

--- contracts/Ring.sol
+++ contracts/Ring.sol
@@ -5,36 +5,7 @@
 
 pragma solidity ^0.4.2;
 
-// ERC Token Standard #20 Interface
-// https://github.com/ethereum/EIPs/issues/20
-contract ERC20Interface {
-    // Get the total token supply
-    function totalSupply() public constant returns (uint256 totalSupplyVal);
-
-    // Get the account balance of another account with address _owner
-    function balanceOf(address _owner) public constant returns (uint256 balance);
-
-    // Send _value amount of tokens to address _to
-    function transfer(address _to, uint256 _value) public returns (bool success);
-
-    // Send _value amount of tokens from address _from to address _to
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
-
-    // Allow _spender to withdraw from your account, multiple times, up to the _value amount.
-    // If this function is called again it overwrites the current allowance with _value.
-    // this function is required for some DEX functionality
-    function approve(address _spender, uint256 _value) public returns (bool success);
-
-    // Returns the amount which _spender is still allowed to withdraw from _owner
-    function allowance(address _owner, address _spender) public constant returns (uint256 remaining);
-
-    // Triggered when tokens are transferred.
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-
-    // Triggered whenever approve(address _spender, uint256 _value) is called.
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-}
-
+import './ERC20.sol';
 
 contract Ring{
     function Ring(uint participants, uint payments, address token) public {
