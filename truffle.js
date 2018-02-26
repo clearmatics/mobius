2289a01bade27462c6f5b8ce8aa59d9f18006942
Antoine Rondelet <rondelet.antoine@gmail.com>
2018-02-26 18:56:11 +0000

    Added documentation to make a payment through Mobius
    Day2 of research
    
    Added readme to make a payment through Mobius

--- truffle.js
+++ truffle.js
@@ -3,24 +3,26 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id 
+      network_id: "*", // Match any network id
+      from: "0x43c719ee71a6212abd2d6d796e589089fdc1e88d",
+      gas: 3112883
     },
     ci: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id          
+      network_id: "*" // Match any network id
     },
     coverage: {
       host: "localhost",
       port: 8555,
       network_id: "*", // Match any network id
       gas: 0xFFFFFFF,
-      gasprice: 0x1        
-    }   
+      gasprice: 0x1
+    }
   },
   mocha: {
     useColors: true,
-    enableTimeouts: false    
+    enableTimeouts: false
   },
   solc: {
     optimizer: {
