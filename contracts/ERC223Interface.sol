ec36da5c7863e0e295a94b76260cd0c3c164bd37
Antoine Rondelet <rondelet.antoine@gmail.com>
2018-03-13 10:44:51 +0000

    Removed useless contracts

--- contracts/ERC223Interface.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.11;
-
-contract ERC223 { 
-    function balanceOf(address who) public constant returns (uint);
-    function transfer(address to, uint value) public;
-    function transfer(address to, uint value, bytes data) public;
-    
-    event Transfer(address from, address to, uint value, bytes data);
-}
--- contracts/ERC223Token.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.4.11;
-
-import './ERC20_functions.sol';
-import './ERC223Interface.sol';
-
-contract ERC223Token is ERC223, ERC20CompatibleToken {
-    // Nothing
-}
