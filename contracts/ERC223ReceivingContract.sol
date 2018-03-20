73d22460840fc3f11552752dddd0ee2d0618059b
Antoine Rondelet <rondelet.antoine@gmail.com>
2018-03-20 11:20:30 +0000

    Removed underscore prefix to arguments of the ERC223 fallback to keep it consistent with the other files

--- contracts/ERC223ReceivingContract.sol
+++ contracts/ERC223ReceivingContract.sol
@@ -16,12 +16,12 @@ contract ERC223ReceivingContract {
         bytes4 sig;
     }
 
-    function tokenFallback(address _from, uint _value, bytes _data) public pure {
+    function tokenFallback(address from, uint value, bytes data) public pure {
         Token memory tkn;
-        tkn.sender = _from;
-        tkn.value = _value;
-        tkn.data = _data;
-        uint32 u = uint32(_data[3]) + (uint32(_data[2]) << 8) + (uint32(_data[1]) << 16) + (uint32(_data[0]) << 24);
+        tkn.sender = from;
+        tkn.value = value;
+        tkn.data = data;
+        uint32 u = uint32(data[3]) + (uint32(data[2]) << 8) + (uint32(data[1]) << 16) + (uint32(data[0]) << 24);
         tkn.sig = bytes4(u);
     }
 }
